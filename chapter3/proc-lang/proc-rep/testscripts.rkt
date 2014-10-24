e526ca2a12823be63618f95690acdca9b60c20d5
rigley007 <rui.ning.1990@gmail.com>
2014-10-24 11:25:32 -0500

    Y-combinator_fibonacci

---
+++ chapter3/proc-lang/proc-rep/testscripts.rkt
@@ -0,0 +1,15 @@
+Welcome to DrRacket, version 6.1 [3m].
+Language: Determine language from source; memory limit: 128 MB.
+> (run-one 'fibonacci-1)
+(num-val 0)
+> (run-one 'fibonacci-2)
+(num-val 1)
+> (run-one 'fibonacci-3)
+(num-val 1)
+> (run-one 'fibonacci-4)
+(num-val 2)
+> (run-one 'fibonacci-5)
+(num-val 3)
+> (run-one 'fibonacci-6)
+(num-val 5)
+> 
